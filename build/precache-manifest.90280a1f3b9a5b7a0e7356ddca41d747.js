722cca885673126c24a9580d8a3b319b6b92865a
Sanjiv shah <93758303+Svshah111@users.noreply.github.com>
2022-12-15 18:03:23 +0545

    Add files via upload

---
+++ build/precache-manifest.90280a1f3b9a5b7a0e7356ddca41d747.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "35f0dc69514d1b9015be",
+    "url": "/React_portfolio-/static/css/main.2bec9574.chunk.css"
+  },
+  {
+    "revision": "35f0dc69514d1b9015be",
+    "url": "/React_portfolio-/static/js/main.35f0dc69.chunk.js"
+  },
+  {
+    "revision": "8453a32ff8b6ffbb93e8",
+    "url": "/React_portfolio-/static/js/1.8453a32f.chunk.js"
+  },
+  {
+    "revision": "384f7ff05c6a44a46c46",
+    "url": "/React_portfolio-/static/js/2.384f7ff0.chunk.js"
+  },
+  {
+    "revision": "1e8150e76db4d04ac38b",
+    "url": "/React_portfolio-/static/js/runtime~main.1e8150e7.js"
+  },
+  {
+    "revision": "08401003586f5162cb8c8f98e9131e9b",
+    "url": "/React_portfolio-/static/media/logo.08401003.png"
+  },
+  {
+    "revision": "b66855815a99e4c9bf3dd8bc004baa20",
+    "url": "/React_portfolio-/static/media/about.b6685581.jpeg"
+  },
+  {
+    "revision": "9fa4dd49296ad0d061b6532ea81779a5",
+    "url": "/React_portfolio-/static/media/service1.9fa4dd49.png"
+  },
+  {
+    "revision": "811b2a6e293e8e6e90b00c0e44a70a0f",
+    "url": "/React_portfolio-/static/media/service2.811b2a6e.png"
+  },
+  {
+    "revision": "83d82e380a4703b04376901fdbddc6ee",
+    "url": "/React_portfolio-/static/media/service3.83d82e38.png"
+  },
+  {
+    "revision": "87bf9f8e67c4cf49dae87ea4db69c011",
+    "url": "/React_portfolio-/index.html"
+  }
+];
